b08783d5806d9da74e3101939fab2fa38e48798a
Simran Singh <simransingh25060@gmail.com>
2024-11-24 22:13:12 +0530

    commit

--- src/game.jsx
+++ src/game.jsx
@@ -8,19 +8,19 @@ const Game = () => {
     <div className="home">
       <div className="section quiz-section">
         <Link to="/quizing" className="link">
-          <img src="https://ucarecdn.com/d145d720-353c-48dd-b509-02b2ee266378/-/preview/1000x1000/" alt="Quiz" className="section-image" id="quiz"/>
+          <img src="https://ucarecdn.com/3a949bbc-8bae-45dd-bc81-094e30c1cdb9/-/preview/1000x571/" alt="Quiz" className="section-image" id="quiz"/>
           <div className="section-title">Quiz</div>
         </Link>
       </div>
       <div className="section puzzle-section">
         <Link to="/puzzle" className="link">
-          <img src="https://ucarecdn.com/a8abc75e-91b1-4699-81fb-eb736bec338e/-/preview/1000x1000/" alt="Puzzle" className="section-image" id="puzzle"/>
+          <img src="https://ucarecdn.com/a2180a8b-9db9-433b-af5b-d8fe2f216819/-/preview/1000x1000/" alt="Puzzle" className="section-image" id="puzzle"/>
           <div className="section-title">Puzzle</div>
         </Link>
       </div>
       <div className="section word-guess-section">
           <Link to="/guess" className="link">
-            <img src="https://ucarecdn.com/3b46dee6-61ad-4aac-832c-c86f2369c643/-/preview/1000x1000/" alt="Word Guess" className="section-image" id="word-guess"/>
+            <img src="https://ucarecdn.com/bd2b761b-4bc3-493b-847c-14976af151c5/-/preview/1000x1000/" alt="Word Guess" className="section-image" id="word-guess"/>
             <div className="section-title">Guess</div>
           </Link>
           </div>
